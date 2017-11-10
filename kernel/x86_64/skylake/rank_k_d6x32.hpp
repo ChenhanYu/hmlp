1b9ab11c1bfb52106fed5cec40ac3f04bf17677f
Chenhan D. Yu <chenhan@cs.utexas.edu>
2017-11-10 16:14:46 -0600

    Add skylake kernels

---
+++ kernel/x86_64/skylake/rank_k_d6x32.hpp
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <hmlp_internal.hpp>
+
+
+/** BLIS kernel prototype declaration */ 
+BLIS_GEMM_KERNEL(bli_sgemm_opt_12x32,float);
+BLIS_GEMM_KERNEL(bli_dgemm_opt_6x32,double);
+
+
+struct rank_k_asm_s12x32
+{
+  const static size_t mr         = 12;
+  const static size_t nr         = 32;
+  const static size_t pack_mr    = 12;
+  const static size_t pack_nr    = 32;
+  const static size_t align_size = 64;
+  const static bool   row_major  = true;
+
+  inline STRA_OPERATOR(float) const
+  {
+    printf( "no STRA_OPERATOR implementation\n" );
+    exit( 1 );
+  };
+
+  inline GEMM_OPERATOR(float) const
+  {
+    float alpha = 1.0;
+    /** if this is the first kc iteration then beta = 1.0 */
+    float beta = aux->pc ? 1.0 : 0.0;
+    /** invoke blis kernel */
+    bli_sgemm_opt_12x32
+    (
+      k,
+      &alpha,
+      a,
+      b,
+      &beta,
+      c, rs_c, cs_c,
+      aux
+    );
+  };
+
+  template<typename TC>
+  inline void operator()                                                
+  (                                                              
+    dim_t k,                                                     
+    float *a,                                                    
+    float *b,                                                    
+    TC    *c,                                                    
+    float *v, inc_t rs_v, inc_t cs_v,                            
+    aux_s<float, float, TC, float> *aux                       
+  )
+  {
+    float alpha = 1.0;
+    /** if this is the first kc iteration then beta = 1.0 */
+    float beta = aux->pc ? 1.0 : 0.0;
+
+    /** allocate temporary buffer */
+    float vtmp[ mr * nr ];
+
+    if ( !is_same<TC, hmlp::MatrixLike<pack_mr, float, float>>::value )
+    {
+      if ( aux->pc )
+      {
+        for ( size_t j = 0; j < aux->jb; j ++ )
+          for ( size_t i = 0; i < aux->ib; i ++ )
+            vtmp[ j * mr + i ] = v[ j * cs_v + i * rs_v ];
+      }
+
+      v = vtmp;
+      rs_v = 1;
+      cs_v = mr;
+    }
+
+    /** invoke blis kernel */
+    bli_sgemm_asm_12x32
+    (
+      k,
+      &alpha,
+      a,
+      b,
+      &beta,
+      v, rs_v, cs_v,
+      reinterpret_cast<aux_s<float, float, float, float>*>( aux )
+    );
+
+    /**
+     *  If TC is not MatrixLike<PACK_MR,double,double>, we treat this
+     *  the same as the edge case.
+     */ 
+    if ( !is_same<TC, hmlp::MatrixLike<pack_mr, float, float>>::value ||
+         aux->ib != mr || aux->jb != nr )
+    {
+      c->Unpack( aux->m, aux->i, aux->ib, aux->n, aux->j, aux->jb, v );
+    }
+
+  };
+
+
+}; /** end struct rank_k_asm_s6x16 */
+
+
+struct rank_k_asm_d6x32 
+{
+  const static size_t mr         =  6;
+  const static size_t nr         = 32;
+  const static size_t pack_mr    =  6;
+  const static size_t pack_nr    = 32;
+  const static size_t align_size = 64;
+  const static bool   row_major  = true;
+
+  inline STRA_OPERATOR(double) const
+  {
+    printf( "no STRA_OPERATOR implementation\n" );
+    exit( 1 );
+  };
+
+  inline GEMM_OPERATOR(double) const
+  {
+    double alpha = 1.0;
+    /** if this is the first kc iteration then beta = 1.0 */
+    double beta = aux->pc ? 1.0 : 0.0;
+    /** invoke blis kernel */
+    bli_dgemm_asm_6x32
+    (
+      k,
+      &alpha,
+      a,
+      b,
+      &beta,
+      c, rs_c, cs_c,
+      aux
+    );
+  };
+
+  template<typename TC>
+  inline void operator()                                                
+  (                                                              
+    dim_t k,                                                     
+    double *a,                                                    
+    double *b,                                                    
+    TC     *c,                                                    
+    double *v, inc_t rs_v, inc_t cs_v,                            
+    aux_s<double, double, TC, double> *aux                       
+  )
+  {
+    double alpha = 1.0;
+    /** if this is the first kc iteration then beta = 1.0 */
+    double beta = aux->pc ? 1.0 : 0.0;
+
+    /** allocate temporary buffer */
+    double vtmp[ mr * nr ];
+
+    if ( !is_same<TC, hmlp::MatrixLike<pack_mr, double, double>>::value )
+    {
+      if ( aux->pc )
+      {
+        for ( size_t j = 0; j < aux->jb; j ++ )
+          for ( size_t i = 0; i < aux->ib; i ++ )
+            vtmp[ j * mr + i ] = v[ j * cs_v + i * rs_v ];
+      }
+
+      v = vtmp;
+      rs_v = 1;
+      cs_v = mr;
+    }
+
+    /** invoke blis kernel */
+    bli_dgemm_asm_6x32
+    (
+      k,
+      &alpha,
+      a,
+      b,
+      &beta,
+      v, rs_v, cs_v,
+      reinterpret_cast<aux_s<double, double, double, double>*>( aux )
+    );
+
+    /**
+     *  If TC is not MatrixLike<PACK_MR,double,double>, we treat this
+     *  the same as the edge case.
+     */ 
+    if ( !is_same<TC, hmlp::MatrixLike<pack_mr, double, double>>::value ||
+         aux->ib != mr || aux->jb != nr )
+    {
+      //printf( "bug %d %d %d %d %d %d\n", aux->m, aux->i, aux->ib, aux->n, aux->j, aux->jb );
+      c->Unpack( aux->m, aux->i, aux->ib, aux->n, aux->j, aux->jb, v );
+    }
+
+  };
+
+}; /** end struct rank_k_asm_d6x32 */
---
+++ package/x86_64/skylake/gnbx.cpp
@@ -0,0 +1,116 @@
+/**
+ *  HMLP (High-Performance Machine Learning Primitives)
+ *  
+ *  Copyright (C) 2014-2017, The University of Texas at Austin
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see the LICENSE file.
+ *
+ **/  
+
+
+/** GNBX templates */
+#include <primitives/gnbx.hpp>
+
+/** Haswell micro-kernels */
+#include <rank_k_d6x32.hpp>
+
+using namespace hmlp;
+
+void gnbx
+(
+	int m, int n, int k,
+	float  *A, int lda,
+  float  *B, int ldb,
+  float  *C, int ldc
+)
+{
+  /** microkernel */
+  rank_k_asm_d6x32 semiringkernel;
+  rank_k_asm_d6x32 microkernel;
+
+  const size_t PACK_MR = rank_k_asm_d6x32::pack_mr; 
+  const size_t PACK_NR = rank_k_asm_d6x32::pack_nr; 
+  const size_t MC = 144;
+  const size_t NC = 5760;
+  const size_t KC = 336;
+
+  /** ObjA */
+  MatrixLike<PACK_MR, float, double> ObjA;
+  ObjA.Set( A, m, k, 1, lda, false );
+
+  /** ObjB */
+  MatrixLike<PACK_NR, float, double> ObjB;
+  ObjB.Set( B, k, n, 1, ldb, true );
+
+  /** ObjC */
+  //MatrixLike<PACK_MR, double, double> ObjC;
+  MatrixLike<PACK_MR, float, double> ObjC;
+  ObjC.Set( C, m, n, 1, ldc, false );
+
+  /** General N-body operator (these 6 types are essential) */
+  gnbx::gnbx<MC, NC, KC, double, double, double>
+  (
+    0, m, n, k,
+    ObjA, 
+    ObjB, 
+    ObjC,
+    semiringkernel,
+    microkernel
+  );
+
+}; /** end gnbx() */
+
+  
+void gnbx
+(
+	int m, int n, int k,
+	double *A, int lda,
+  double *B, int ldb,
+  double *C, int ldc
+)
+{
+  /** microkernel */
+  rank_k_asm_s12x32 semiringkernel;
+  rank_k_asm_s12x32 microkernel;
+
+  const size_t PACK_MR = rank_k_asm_s12x32::pack_mr; 
+  const size_t PACK_NR = rank_k_asm_s12x32::pack_nr; 
+  const size_t MC = 480;
+  const size_t NC = 3072;
+  const size_t KC = 384;
+
+  /** ObjA, stored in double, computed in float */
+  MatrixLike<PACK_MR, double, float> ObjA;
+  ObjA.Set( A, m, k, 1, lda, false );
+
+  /** ObjB, stored in double, computed in float */
+  MatrixLike<PACK_NR, double, float> ObjB;
+  ObjB.Set( B, k, n, 1, ldb, true );
+
+  /** ObjC, stored in double, computed in float */
+  MatrixLike<PACK_MR, double, float> ObjC;
+  ObjC.Set( C, m, n, 1, ldc, false );
+
+  /** General N-body operator (these 6 types are essential) */
+  gnbx::gnbx<MC, NC, KC, float, float, float>
+  (
+    0, m, n, k,
+    ObjA, 
+    ObjB, 
+    ObjC,
+    semiringkernel,
+    microkernel
+  );
+
+}; /** end gnbx() */
